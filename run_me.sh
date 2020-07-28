203d5b96df2764cd2a3c353315e7f450d57df984
zac-j-harris <54678510+zac-j-harris@users.noreply.github.com>
2020-07-27 20:51:09 -0400

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -26,40 +26,44 @@ hwclock --systohc
 sed 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "What is your quest?" && read HOSTNAME
+# root passwd
+echo "What... is your computer's quest? (Hostname)" && read HOSTNAME
 echo $HOSTNAME > /etc/hostname
+echo "What... is the capital of Assyria? (Root Password)" && read ROOTPASS
+echo "root:$ROOTPASS" | chpasswd
 # vim /etc/hosts
 echo -e "127.0.0.1   localhost\n::1     localhost\n127.0.1.1   $HOSTNAME.localdomain  $HOSTNAME" >> /etc/hosts
 
-passwd
+# usr passwd
+echo "What... is your quest? (Username)" && read USRNAME
+echo "What... is your favorite color? (User Password)" && read USRPASS
 groupadd sudo
-useradd -m -g users -G wheel -s /bin/bash you
-passwd you
+useradd -m -g users -G wheel -s /bin/bash $USRNAME
+echo "$USRNAME:$USRPASS" | chpasswd
 usermod -aG sudo root
-usermod -aG sudo you
-vim /etc/sudoers
-vim /etc/modules
+usermod -aG sudo $USRNAME
+# vim /etc/sudoers
+echo "%sudo      ALL = (ALL) ALL" > /etc/sudoers
+%wheel      ALL = (ALL) ALL
 
-coretemp
-applesmc
+# vim /etc/modules
+echo -e "coretemp\napplesmc" > /etc/modules
 
 bootctl --path=/boot install
-vim /boot/loader/entries/arch.conf
 
-title Arch Linux
-linux /vmlinuz-linux
-initrd /intel-ucode.img
-initrd /initramfs-linux.img
-options root=/dev/sda1 intel_iommu=on
+# vim /boot/loader/entries/arch.conf
 
-vim /boot/loader/loader.conf
+echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /intel-ucode.img\ninitrd /initramfs-linux.img\noptions root=/dev/sda1 intel_iommu=on" > /boot/loader/entries/arch.conf
 
-Comment whatever isnt
-default arch-*
+# vim /boot/loader/loader.conf
+
+echo "default arch-*" > /boot/loader/loader.conf
 
 exit
-umount -R /mnt
-reboot
+
+# umount -R /mnt
+
+# reboot
 
 
 
